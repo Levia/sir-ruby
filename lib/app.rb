8c4f043cc3e6b3bd0e5e42c734983f52e472ee8c
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2020-04-29 21:30:26 +0100

    Fix indentation

--- lib/app.rb
+++ lib/app.rb
@@ -4,9 +4,9 @@ require 'csv'
 
 module App
   def self.run
-		simulation = SimulationFactory.make(read_states, 0.2)
+    simulation = SimulationFactory.make(read_states, 0.2)
 
-		puts simulation.run(1)[1]
+    puts simulation.run(1)[1]
   end
 
   private
